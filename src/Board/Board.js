8095e3fdf3e981ff0b2a4c45d4242c6d13e0cffe
Heather Hartley <hlhartley@gmail.com>
2020-04-05 11:09:08 -0600

    Build out Board

---
+++ src/Board/Board.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import '../main.scss';
+
+function Board() {
+  return (
+    <div className="Board">
+      <article className="words__row">
+        <section>word 1</section>
+        <section>word 2</section>
+        <section>word 3</section>
+        <section>word 4</section>
+        <section>word 5</section>
+      </article>
+      <article className="words__row">
+        <section>word 6</section>
+        <section>word 7</section>
+        <section>word 8</section>
+        <section>word 9</section>
+        <section>word 10</section>
+      </article>
+      <article className="words__row">
+        <section>word 6</section>
+        <section>word 7</section>
+        <section>word 8</section>
+        <section>word 9</section>
+        <section>word 10</section>
+      </article>
+      <article className="words__row">
+        <section>word 11</section>
+        <section>word 12</section>
+        <section>word 13</section>
+        <section>word 14</section>
+        <section>word 15</section>
+      </article>
+      <article className="words__row">
+        <section>word 16</section>
+        <section>word 17</section>
+        <section>word 18</section>
+        <section>word 19</section>
+        <section>word 20</section>
+      </article>
+    </div>
+  );
+}
+
+export default Board;
