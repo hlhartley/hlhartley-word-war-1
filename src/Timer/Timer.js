74cb5e4253da8c187aa290aeb0bc1f9de71c5a55
Heather Hartley <hlhartley@gmail.com>
2020-04-08 14:04:12 -0600

    Add Timer component

--- src/Timer/Timer.js
+++ src/Timer/Timer.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../main.scss';
 
 function Timer() {
+  const [countDown, setCountDown] = useState(30);
+  const [timerOn, toggleTimer] = useState(true);
+
   return (
     <div className="Timer">
-      Timer
+      Timer: <span>{countDown}</span> seconds
+      <button type="button" className={timerOn ? "btn btn-outline-success" : "btn btn-outline-danger"}>{timerOn ? "start" : "stop"}</button>
     </div>
   );
 }
