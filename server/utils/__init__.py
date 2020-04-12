11f808aa9c22390e21f32edfdb92887b9125e605
leepuppychow <leepuppychow@gmail.com>
2020-04-11 22:13:49 -0600

    Moves more into queries file

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -3,5 +3,5 @@ from .db import db_connection
 from .mapping import map_game_words, map_players
 from .queries import get_current_turn, get_game_words, get_game_players, \
   add_player_to_game, delete_player, check_existing_spymaster, create_game, \
-  get_word_guess, mark_word_as_guessed
+  get_word_guess, mark_word_as_guessed, populate_words, change_turns
 from .game import get_each_teams_players, get_remaining_words, check_winner
--- server/utils/queries.py
+++ server/utils/queries.py
@@ -1,10 +1,37 @@
-from .constants import TEAM_1
+import random
+from .constants import BOMB, TEAM_1, TEAM_2, NEUTRAL
 
 def create_game(conn):
   cur = conn.cursor()
   try:
     cur.execute("INSERT INTO game (turn) values (%s) RETURNING id", (TEAM_1,))
     return cur.fetchone()[0]
+  except Exception:
+    conn.rollback()
+
+def populate_words(conn, id_game):
+  cur = conn.cursor()
+  classifiers = [BOMB] + [TEAM_1]*9 + [TEAM_2]*8 + [NEUTRAL]*7
+  random.shuffle(classifiers)
+  
+  try:
+    cur.execute("SELECT * FROM word ORDER BY RANDOM() LIMIT 25")
+    words = cur.fetchall()
+    game_words = []
+  
+    for i, word in enumerate(words):
+      classifier = classifiers.pop()
+      cur.execute(
+        "INSERT INTO game_word (id_game, id_word, classifier, is_guessed) VALUES (%s, %s, %s, %s) RETURNING id",
+        (id_game, word[0], classifier, False)
+      )
+      game_words.append({
+        "id": cur.fetchone()[0],
+        "text": word[1],
+        "classifier": classifier,
+        "is_guessed": False,
+      })
+    return game_words   
   except Exception:
     conn.rollback()
 
@@ -15,6 +42,18 @@ def get_current_turn(conn, id_game):
     return cur.fetchone()[0]
   except Exception:
     conn.rollback()
+
+def change_turns(conn, id_game, current_team, game_over=False):
+    if game_over:
+      turn = 'game_over'
+    else:
+      turn = TEAM_2 if current_team == TEAM_1 else TEAM_1
+    
+    cur = conn.cursor()
+    try:
+      cur.execute("UPDATE game SET turn = %s WHERE id = %s", (turn, id_game))
+    except Exception:
+      conn.rollback()
 
 def get_game_words(conn, id_game):
   cur = conn.cursor()
