86f02e7db4607a210d3e0816622071c270b162b2
leepuppychow <leepuppychow@gmail.com>
2020-04-11 22:14:41 -0600

    Refactor populate_words

--- server/lambdas/https/create_game.py
+++ server/lambdas/https/create_game.py
@@ -1,40 +1,11 @@
 import json
-import random
-from utils import db_connection, BOMB, TEAM_1, TEAM_2, NEUTRAL, create_game
+from utils import db_connection, TEAM_1, create_game, populate_words
 
 conn = db_connection()
 
-
-def _populate_words(idGame):
-  cur = conn.cursor()
-  classifiers = [BOMB] + [TEAM_1]*9 + [TEAM_2]*8 + [NEUTRAL]*7
-  random.shuffle(classifiers)
-  
-  try:
-    cur.execute("SELECT * FROM word ORDER BY RANDOM() LIMIT 25")
-    words = cur.fetchall()
-    game_words = []
-  
-    for i, word in enumerate(words):
-      classifier = classifiers.pop()
-      cur.execute(
-        "INSERT INTO game_word (id_game, id_word, classifier, is_guessed) VALUES (%s, %s, %s, %s) RETURNING id",
-        (idGame, word[0], classifier, False)
-      )
-      game_words.append({
-        "id": cur.fetchone()[0],
-        "text": word[1],
-        "classifier": classifier,
-        "is_guessed": False,
-      })
-    return game_words
-    
-  except Exception:
-    conn.rollback()
-
 def lambda_handler(event, context):
   id_game = create_game(conn)
-  game_words = _populate_words(id_game)
+  game_words = populate_words(conn, id_game)
   conn.commit()
   
   body = {
