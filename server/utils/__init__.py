ca7d55ec84072b7f56a19c07cc3f93ff067bdb51
leepuppychow <leepuppychow@gmail.com>
2020-04-16 19:58:15 -0600

    Import

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -3,5 +3,6 @@ from .db import db_connection
 from .mapping import map_game_words, map_players
 from .queries import get_current_turn, get_game_words, get_game_players, \
   add_player_to_game, delete_player, check_existing_spymaster, create_game, \
-  get_word_guess, mark_word_as_guessed, populate_words, change_turns
+  get_word_guess, mark_word_as_guessed, populate_words, change_turns, \
+  add_connection_to_game
 from .game import get_each_teams_players, get_remaining_words, check_winner
