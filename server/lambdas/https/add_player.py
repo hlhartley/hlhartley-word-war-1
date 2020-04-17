468cd31c77d84c75edf01e47622905eceba4457c
leepuppychow <leepuppychow@gmail.com>
2020-04-16 19:59:01 -0600

    Add player based on gameId and connectionId (unique)

--- server/lambdas/https/add_player.py
+++ server/lambdas/https/add_player.py
@@ -19,6 +19,7 @@ def lambda_handler(event, context):
     
     if queryStringParameters:
       is_spymaster = queryStringParameters.get('is_spymaster', False)
+      connection_id = queryStringParameters.get('connection_id', None)
       
     if is_spymaster and check_existing_spymaster(conn, id_game, team):
       return {
@@ -27,7 +28,7 @@ def lambda_handler(event, context):
         'body': json.dumps({'message': f'Spymaster already set for {team}'}),
       }
     else:
-      add_player_to_game(conn, id_game, id_player, is_spymaster, team)
+      add_player_to_game(conn, id_game, id_player, is_spymaster, team, connection_id)
       conn.commit()
       return {
           'statusCode': 200,
