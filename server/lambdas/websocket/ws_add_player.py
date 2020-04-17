50f5d61b3f01cdb3f6223cef23e970878b9ad485
leepuppychow <leepuppychow@gmail.com>
2020-04-17 09:11:48 -0600

    Add try except here

--- server/lambdas/websocket/ws_add_player.py
+++ server/lambdas/websocket/ws_add_player.py
@@ -9,8 +9,12 @@ def lambda_handler(event, context):
     connection_id = event.get("requestContext").get('connectionId')
     body = json.loads(event.get("body"))
     id_game = body.get('gameId')
-    add_connection_to_game(conn, id_game, connection_id)
-    conn.commit()
+    
+    try:
+      add_connection_to_game(conn, id_game, connection_id)
+      conn.commit()
+    except Exception:
+      conn.rollback()
     
     data = {
       "connection_id": connection_id,
