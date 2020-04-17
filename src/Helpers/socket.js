e1a082659928bd7028c2549ea3c077cecaf4fc56
leepuppychow <leepuppychow@gmail.com>
2020-04-17 04:40:06 -0600

    Refactor socket service

--- src/Helpers/socket.js
+++ src/Helpers/socket.js
@@ -2,14 +2,8 @@ const SOCKET_URL = 'wss://emkh1mv1x1.execute-api.us-west-2.amazonaws.com/dev/';
 
 export const socket = new WebSocket(SOCKET_URL);
 
-export const addPlayer = (gameId, playerId, team, isSpymaster) => {
-  socket.send(JSON.stringify({
-    action: "addPlayer", 
-    gameId,
-    playerId,
-    team,
-    isSpymaster,
-  }))  
+export const createPlayerConnection = (gameId) => {
+  socket.send(JSON.stringify({ action: "addPlayer", gameId }))  
 };
 
 export const removePlayer = () => {
@@ -17,8 +11,5 @@ export const removePlayer = () => {
 };
 
 export const getGameData = (gameId) => {
-  socket.send(JSON.stringify({ 
-    action: "getGameData", 
-    gameId, 
-  }))    
+  socket.send(JSON.stringify({ action: "getGameData", gameId }))    
 };
