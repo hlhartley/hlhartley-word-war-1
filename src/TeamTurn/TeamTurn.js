ee8980b55185dd0789a136ed56f3d6bf1ec23034
leepuppychow <leepuppychow@gmail.com>
2020-04-18 09:06:24 -0600

    Fixes warning with HTML attr 'class' in react

--- src/TeamTurn/TeamTurn.js
+++ src/TeamTurn/TeamTurn.js
@@ -4,7 +4,7 @@ import '../main.scss';
 function TeamTurn(props) {
   return (
     <div className="TeamTurn">
-      <span className={props.teamTurn === "team_1" ? "team_1 team-turn__text" : "team_2 team-turn__text"}><i class="fas fa-user-friends"></i>'s Turn</span>
+      <span className={props.teamTurn === "team_1" ? "team_1 team-turn__text" : "team_2 team-turn__text"}><i className="fas fa-user-friends"></i>'s Turn</span>
     </div>
   );
 }
