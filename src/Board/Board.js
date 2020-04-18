e6b67f8935759af01285ff8a69cd9cead4548b09
leepuppychow <leepuppychow@gmail.com>
2020-04-17 23:27:30 -0600

    Adjusts board's display for player vs spymaster

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -4,17 +4,38 @@ import { makeGuess } from '../Helpers/requests';
 import { getGameData } from '../Helpers/socket';
 
 function Board(props) {
-  const { gameId, team } = props;
+  const { gameId, team, isSpymaster } = props;
+ 
   async function guessWord(word) {
-    await makeGuess({ gameId, team, word });
+    if (word.is_guessed || isSpymaster) {
+      return;
+    }
+    await makeGuess({ gameId, team, word: word.text });
     getGameData(gameId);
+  }
+
+  function sectionClass(word) {
+    const classes = [];
+    
+    if (word.is_guessed && isSpymaster) {
+      classes.push('spymaster__guessed');
+    }
+    if (word.is_guessed || isSpymaster) {
+      classes.push(word.classifier);
+    }
+
+    return classes.join(' ');
   }
 
   return (
     <div className="Board">
       <div className="words__row">
         {props.words.map((word) => {
-          return <section key={word.text} className={word.is_guessed ? word.classifier : ''} onClick={() => guessWord(word.text)}>{word.text}</section>
+          return (
+            <section key={word.text} className={sectionClass(word)} onClick={() => guessWord(word)}>
+              {word.text}
+            </section>
+          )
         })}
       </div>
     </div>
